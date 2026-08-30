520df9ce4aab9da56f2f1b840aa8c647e872ba21
infomuhammadayan77 <info.muhammadayan77@gmail.com>
2026-08-30 15:19:30 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,25 @@
 module.exports = {
-    startimage: 'https://i.postimg.cc/W3MK0wgJ/Chat-GPT-Image-Aug-26-2026-03-06-36-PM.png',
-    ownerNumber: process.env.OWNER_NUMBER || '923000000000',
-    botName: 'MA BOT',
-    ownerName: 'Muhammad Ayan',
-    ownerImage: 'https://i.postimg.cc/050FQZ89/Chat-GPT-Image-Aug-26-2026-03-07-12-PM.png',
-    footer: '© MA Developers',
-    copyright: 'Muhammad Ayan',
-    whatsappChannel: 'https://whatsapp.com/channel/0029VaGiJKfIiRoybPBMTy38',
-    tgOwnerId: process.env.OWNER_TELEGRAM_ID || '8488081516',
-    premiumUsers: [],
-    connectedBots: [],
-    version: '1.0.0',
-    prefix: '.'
+    botName: "MA BOT",
+    ownerName: "Muhammad Ayan",
+    developerName: "MA Developers",
+
+    prefix: ".",
+
+    port: process.env.PORT || 3000,
+
+    logo:
+        "https://i.postimg.cc/050FQZ89/Chat-GPT-Image-Aug-26-2026-03-07-12-PM.png",
+
+    commands: {
+        ping: true,
+        uptime: true,
+        calc: true,
+        joke: true,
+        fact: true,
+        quote: true,
+        numberinfo: true,
+        whatsappinfo: true,
+        groupinfo: true,
+        tagall: true
+    }
 };
