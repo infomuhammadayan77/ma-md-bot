cae424db2c5473b70c3ce1a2b49c5fe9b10a63b8
infomuhammadayan77 <info.muhammadayan77@gmail.com>
2026-08-30 10:34:47 +0500

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+    startimage: 'https://i.postimg.cc/W3MK0wgJ/Chat-GPT-Image-Aug-26-2026-03-06-36-PM.png',
+    ownerNumber: process.env.OWNER_NUMBER || '923000000000',
+    botName: 'MA BOT',
+    ownerName: 'Muhammad Ayan',
+    ownerImage: 'https://i.postimg.cc/050FQZ89/Chat-GPT-Image-Aug-26-2026-03-07-12-PM.png',
+    footer: '© MA Developers',
+    copyright: 'Muhammad Ayan',
+    whatsappChannel: 'https://whatsapp.com/channel/0029VaGiJKfIiRoybPBMTy38',
+    tgOwnerId: process.env.OWNER_TELEGRAM_ID || '8488081516',
+    premiumUsers: [],
+    connectedBots: [],
+    version: '1.0.0',
+    prefix: '.'
+};
